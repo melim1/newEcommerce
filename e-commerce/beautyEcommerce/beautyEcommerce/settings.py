c7916623f45c04e1e5c70f6edc45bd00771e0261
melim1 <rmelissa.remila@se.univ-bejaia.dz>
2025-05-20 16:25:32 +0100

    Update settings.py

--- e-commerce/beautyEcommerce/beautyEcommerce/settings.py
+++ e-commerce/beautyEcommerce/beautyEcommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%i$!fqh6@(c7pqd(v#-a7#67sg#ih)w0hd+upgbh@k4fdb%cqt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
